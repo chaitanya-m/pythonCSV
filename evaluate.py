765164056a559dce0b12c9275ce00a672aadfa0c
chaitanya-m <c.manapragada@gmail.com>
2014-08-09 11:26:08 +1000

    Extract read values from reader object, store them.

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,37 @@
-import csv
-with open('Test1.csv', 'rb') as csvfile:
-    dataReader = csv.reader(csvfile, delimiter=',')
-    for row in dataReader:
-        print ', '.join(row)
+import sys, csv
+
+inputData=[]
+
+def main():
+    getInputData()
+    evaluateExpressions()    
+
+def getInputData():
+    with open(sys.argv[1], 'rb') as csvfile:
+        global inputData 
+        dataReader = csv.reader(csvfile, delimiter=',')
+        print "\nData read was:"
+        for row in dataReader:
+            inputData.append(row)
+            print ', '.join(row)            
+
+def evaluateExpressions():
+    global inputData
+
+    print "\nData stored was:"
+    for row in inputData:
+        print row
+
+    for row in inputData:
+        for element in row:
+            print element
+
+
+
+if __name__ == '__main__':
+    main()
+#Apparently this is a Pythonic idiom- StackOverflow.
+#Code in functions is compiled to bytecode.
+#This means local variables are stord in fixed-size arrays, not dicts.
+#This makes lookups faster!
+
