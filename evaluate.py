b51be9766d9721b076734efbe507f42bf1c4e7da
chaitanya-m <c.manapragada@gmail.com>
2014-08-09 13:28:43 +1000

    addition works for integers

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,15 @@
-import sys, csv
+import csv, sys, re
+from decimal import Decimal
 
 inputData=[]
 
+#validExpressions={}
+#validExpressions[
+addition = re.compile('(\d+) (\d+) \+')
+
 def main():
     getInputData()
-    evaluateExpressions()    
+    processSpreadsheet()    
 
 def getInputData():
     with open(sys.argv[1], 'rb') as csvfile:
@@ -15,23 +20,33 @@ def getInputData():
             inputData.append(row)
             print ', '.join(row)            
 
-def evaluateExpressions():
+def processSpreadsheet():
     global inputData
 
     print "\nData stored was:"
     for row in inputData:
         print row
 
+    print "\nValues evaluate to:"
     for row in inputData:
-        for element in row:
-            print element
+        for cell in row:
+            cellValue = evaluateExpression(cell)
+            print cellValue
 
+def evaluateExpression(expression):
+
+    match = addition.match(expression)
+    if match is not None:
+        return add( Decimal(match.group(1)), Decimal(match.group(2)) )
+
+def add(summand1, summand2):
+    return summand1 + summand2
 
 
 if __name__ == '__main__':
     main()
 #Apparently this is a Pythonic idiom- StackOverflow.
 #Code in functions is compiled to bytecode.
-#This means local variables are stord in fixed-size arrays, not dicts.
+#This means local variables are stored in fixed-size arrays, not dictionaries.
 #This makes lookups faster!
 
